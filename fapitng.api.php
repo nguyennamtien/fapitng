41aaa4bfda16e6f1fc696f68fac659527664d0b1
Bart Feenstra <bartfeenstra@nederdev.nl>
2010-10-03 01:54:32 +0200

    Fix indenting.

--- fapitng.api.php
+++ fapitng.api.php
@@ -5,12 +5,12 @@
  * Alter any form in its build stage.
  */
 function hook_fapitng_form_alter(fapitng_Form $form) {
-	
+  
 }
 
 /**
  * Alter a specific form in its build stage.
  */
 function hook_fapitng_form_BUILD_CALLBACK_alter(fapitng_Form $form) {
-	
+  
 }
