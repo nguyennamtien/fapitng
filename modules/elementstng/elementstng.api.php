002437934d400203d1b149f2b8825e11a245637e
Bart Feenstra <bartfeenstra@nederdev.nl>
2010-10-25 03:01:59 +0200

    Added hook_elementstng_element_properties(), the replacement for hook_element_info_alter().

--- modules/elementstng/elementstng.api.php
+++ modules/elementstng/elementstng.api.php
@@ -2,23 +2,12 @@
 // $Id$
 
 /**
- * Define renderable elements.
- *
- * Even though the basic implementation only requires a machine name and a
- * class, a format has been chosen to allow other modules to extend the
- * information by adding other properties, like titles, as you can see in
- * hook_elementstng_element_info_alter();
+ * Allow modules to add properties to elements or overwrite existing ones.
  */
-function hook_elementstng_element_info() {
-  $elements['form'] = array(
-    'class' => 'fapitng_Form'
+function hook_elementstng_element_properties() {
+  $properties['form'] = array(
+    'submit_callbacks' => array('foo_custom_submit_callback')
   );
-  $elements['markup'] = array(
-    'class' => 'elementstng_Markup'
-  );
-}
 
-function hook_elementstng_element_info_alter(array &$elements) {
-  $elements['markup']['class'] = 'foobar_Markup';
-  $elements['markup']['title'] = t('Markup');
+  return $properties;
 }
